4a98b6026bf161b92fb2295bc59c7a047c9794c2
Rakapurbayu <rakapurbayu.tito.aryasaputra-2022@vokasi.unair.ac.id>
2023-07-13 14:22:04 +0700

    comit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\post; //load Post model
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    {
+        $assignment= Assignment::latest()->get();
+        return view ('assignment.index',compact('assignment'));
+    }
+}
+public function create()
+{
+return view('posts.create');
 }
