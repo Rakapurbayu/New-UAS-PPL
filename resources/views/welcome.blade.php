c660fce7a874c9398d0f9431d886615e6d8f1fb2
lintangsekar <134347531+lintangsekar@users.noreply.github.com>
2023-07-19 04:20:12 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,49 +68,57 @@
           </button>
         </div>
 
-        <ul class="navbar-list">
+       <ul class="navbar-list">
+  <li class="navbar-item">
+    <a href="#home" class="navbar-link" data-nav-link style="color: black;" onmouseover="this.style.color='#C4A0ED'" onmouseout="this.style.color='black'">Home</a>
+  </li>
 
-          <li class="navbar-item">
-            <a href="#home" class="navbar-link" data-nav-link>Home</a>
-          </li>
+  <li class="navbar-item">
+    <a href="#about" class="navbar-link" data-nav-link style="color: black;" onmouseover="this.style.color='#C4A0ED'" onmouseout="this.style.color='black'">About</a>
+  </li>
 
-          <li class="navbar-item">
-            <a href="#about" class="navbar-link" data-nav-link>About</a>
-          </li>
+  <li class="navbar-item">
+    <a href="#courses" class="navbar-link" data-nav-link style="color: black;" onmouseover="this.style.color='#C4A0ED'" onmouseout="this.style.color='black'">Courses</a>
+  </li>
 
-          <li class="navbar-item">
-            <a href="#courses" class="navbar-link" data-nav-link>Courses</a>
-          </li>
+  <li class="navbar-item">
+    <a href="#blog" class="navbar-link" data-nav-link style="color: black;" onmouseover="this.style.color='#C4A0ED'" onmouseout="this.style.color='black'">Blog</a>
+  </li>
 
-          <li class="navbar-item">
-            <a href="#blog" class="navbar-link" data-nav-link>Blog</a>
-          </li>
+  <li class="navbar-item">
+    <a href="#" class="navbar-link" data-nav-link style="color: black;" onmouseover="this.style.color='#C4A0ED'" onmouseout="this.style.color='black'">Contact</a>
+  </li>
+</ul>
 
-          <li class="navbar-item">
-            <a href="#" class="navbar-link" data-nav-link>Contact</a>
-          </li>
-
-        </ul>
 
       </nav>
 
       <div class="header-actions">
+	
 
-        <button class="header-action-btn" aria-label="toggle search" title="Search">
-          <ion-icon name="search-outline" aria-hidden="true"></ion-icon>
-        </button>
+        <style>
+  /* Add this style block inside the head tag or in a style section */
+  .header-action-btn:hover ion-icon[name="search-outline"] {
+    filter: invert(73%) sepia(32%) saturate(2499%) hue-rotate(271deg) brightness(104%) contrast(102%);
+  }
+</style>
+
+<button class="header-action-btn" aria-label="toggle search" title="Search">
+  <ion-icon name="search-outline" aria-hidden="true"></ion-icon>
+</button>
+
 
         <button class="header-action-btn" aria-label="cart" title="Cart">
           <ion-icon name="cart-outline" aria-hidden="true"></ion-icon>
 
           <span class="btn-badge">0</span>
         </button>
+<a href="#" class="btn has-before" style="background-color: #C4A0ED; color: white;">
+  <span class="span">Try for free</span>
+  
+  <ion-icon name="arrow-forward-outline" aria-hidden="true"></ion-icon>
+</a>
 
-        <a href="#" class="btn has-before">
-          <span class="span">Try for free</span>
-
-          <ion-icon name="arrow-forward-outline" aria-hidden="true"></ion-icon>
-        </a>
 
         <button class="header-action-btn" aria-label="open menu" data-nav-toggler>
           <ion-icon name="menu-outline" aria-hidden="true"></ion-icon>
